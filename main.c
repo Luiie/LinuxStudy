4ec0e6c9374dd5a0e93137b9c054dbcbdc6a399e
luiie <dbsldkfls@ewhain.net>
2025-10-06 13:09:46 +0900

    Add a Hanix Device Driver

--- main.c
+++ main.c
@@ -1,18 +1,38 @@
 #include <linux/module.h>
+#include <linux/fs.h>
 
-static int __init hanix_module_init(void)
+#define HANIX_DEVICE_NAME "hanix-device"
+#define HANIX_MAJOR_NUMBER 177
+
+static int hanuixDevice_open(struct inode *inode, struct file *fp)
 {
-    printk(KERN_DEBUG "%s\n", __func__);
+    // Get the Minor number from the inode structure
+    int minor = iminor(inode);
+    printk(KERN_DEBUG "%s - minor : %d\n", __func__, minor); 
+    
     return 0;
 }
 
-static void __exit hanix_module_exit(void)
+static struct file_operations hanuixDevice_fileOperations = {
+    .open = hanuixDevice_open,
+};
+
+static int __init hanixModule_init(void)
 {
     printk(KERN_DEBUG "%s\n", __func__);
+    return register_chrdev(HANIX_MAJOR_NUMBER,
+                           HANIX_DEVICE_NAME,
+                           &hanuixDevice_fileOperations);
 }
 
-module_init(hanix_module_init);
-module_exit(hanix_module_exit);
+static void __exit hanixModule_exit(void)
+{
+    printk(KERN_DEBUG "%s\n", __func__);
+    unregister_chrdev(HANIX_MAJOR_NUMBER, HANIX_DEVICE_NAME);
+}
+
+module_init(hanixModule_init);
+module_exit(hanixModule_exit);
 
 
 MODULE_AUTHOR("Luiie<dbsldkfls@naver.com>");
